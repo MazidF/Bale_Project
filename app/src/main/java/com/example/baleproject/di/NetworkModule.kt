b0039c596877d84d8a28f017d0d773bb0368bf3b
m.m <m3farahani1382@gmail.com>
2022-08-30 12:17:37 +0430

    setup retrofit

---
+++ app/src/main/java/com/example/baleproject/di/NetworkModule.kt
@@ -0,0 +1,81 @@
+package com.example.baleproject.di
+
+import com.example.baleproject.di.qualifiers.Authorizer
+import com.example.baleproject.di.qualifiers.Logger
+import com.example.baleproject.utils.SERVER_BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    @Authorizer
+    fun provideAuthorizerInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val oldRequest = chain.request()
+
+            val newUrl = oldRequest.url().newBuilder()
+                .build()
+
+            val newRequest = chain.request().newBuilder()
+                .url(newUrl)
+                .build()
+
+            chain.proceed(newRequest)
+        }
+    }
+
+    @Provides
+    @Singleton
+    @Logger
+    fun provideHttpLoggingInterceptor(): Interceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        @Logger logger: Interceptor,
+        @Authorizer authorizer: Interceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .addInterceptor(authorizer)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        gson: Gson,
+        client: OkHttpClient,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(SERVER_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
+    }
+}
---
+++ app/src/main/java/com/example/baleproject/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.baleproject.utils
+
+const val SERVER_BASE_URL = "/"
